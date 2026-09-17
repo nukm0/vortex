d9815a2a3bf71c4aefa962fc2627f0355a7b7b55
nukm0 <netankyu@gmail.com>
2026-09-17 12:38:54 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ru">
-      <body>{children}</body>
+      <body className="min-h-screen">{children}</body>
     </html>
   );
 }
