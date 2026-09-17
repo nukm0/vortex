6142d478d4a4ac96adc4145ef69de5c5314b67a5
nukm0 <netankyu@gmail.com>
2026-09-17 11:44:59 +0300

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,19 @@
+import "./globals.css";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Admin Panel",
+  description: "Учёт товаров, продавцов и ревизий",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="ru">
+      <body>{children}</body>
+    </html>
+  );
+}
